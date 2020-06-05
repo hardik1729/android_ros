03aff9d5b37497eb7d988527ef92c374e91ccc86
hardik1729 <hardikjain3101@gmail.com>
2020-06-05 13:43:16 +0530

    first commit

---
+++ src/android_core/android_sensors_driver/buildAll.sh
@@ -0,0 +1,11 @@
+# Bash
+
+../gradlew clean
+if [ $? -eq 0 ]
+ then
+  adb uninstall org.ros.android.android_sensors_driver
+  adb install bin/MainActivity-debug.apk
+  adb shell am start -n org.ros.android.android_sensors_driver/org.ros.android.android_sensors_driver.MainActivity
+  adb logcat -c
+  adb logcat
+fi
